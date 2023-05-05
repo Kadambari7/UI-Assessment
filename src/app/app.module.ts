499bbb1af5a760aadb18fc81d7b616d33bad6960
kadambari7 <kadambarimore752000@gmail.com>
2023-05-05 20:57:09 +0530

    commit 2 appcode change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContainerComponent } from './Container/container.component';
+import { NavComponent } from './nav/nav.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { Header82Component } from './header82/header82.component';
+import { Header23Component } from './header23/header23.component';
+import { PropertyComponent } from './property/property.component';
+import { CTAComponent } from './cta/cta.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { CTA1Component } from './cta1/cta1.component';
+import { FeatureComponent } from './feature/feature.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContainerComponent,
+    NavComponent,
+    SearchBarComponent,
+    Header82Component,
+    Header23Component,
+    PropertyComponent,
+    CTAComponent,
+    CarouselComponent,
+    CTA1Component,
+    FeatureComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
